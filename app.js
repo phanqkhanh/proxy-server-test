f4abc4db9575cc080316a048a9aaf4d80662b0aa
khanh <phanquockhanh9196@gmail.com>
2023-10-25 09:21:18 +0700

    push

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080
 
+app.get('/user', (req, res) => {
+  res.json({ message: 'Get user' })
+})
+
+app.post('/', (req, res) => {
+  res.json({ message: 'Post' })
+})
+
 app.get('/', (req, res) => {
   res.json({ message: 'Hello world!' + process.env.NOTE })
 })
